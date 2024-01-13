3d0dd90e4256d3f21b0ba9d40364a1200f3bfbf4
jooyoung9939 <155051230+jooyoung9939@users.noreply.github.com>
2024-01-13 15:17:36 +0900

    구글 api 라벨 인식 구현 완료

--- madcamp3/lib/main.dart
+++ madcamp3/lib/main.dart
@@ -44,6 +44,7 @@ class VisionApiExample extends StatefulWidget {
 class _VisionApiExampleState extends State<VisionApiExample> {
   File? _selectedImage;
   String _extractedText = '';
+  List<String?> _labels = [];
 
 
   Future<void> _selectImage() async {
@@ -57,12 +58,14 @@ class _VisionApiExampleState extends State<VisionApiExample> {
         _extractedText =
         ''; // Reset extracted text when a new image is selected
       });
-      _extractTextFromImage();
+      _extractTextAndLabelsFromImage();
     }
   }
 
-  Future<void> _extractTextFromImage() async {
+
+  Future<void> _extractTextAndLabelsFromImage() async {
     if (_selectedImage == null) return;
+
 
     print('Selected Image Path: ${_selectedImage!.path}');
 
@@ -86,7 +89,8 @@ class _VisionApiExampleState extends State<VisionApiExample> {
                 'content': base64Encode(imageBytes),
               },
               'features': [
-                {'type': 'TEXT_DETECTION'}
+                {'type': 'TEXT_DETECTION'},
+                {'type': 'LABEL_DETECTION'}
               ],
             },
           ],
@@ -97,7 +101,9 @@ class _VisionApiExampleState extends State<VisionApiExample> {
         // Process results for each image, assuming only one image is requested
         final annotateImageResponse = response.responses![0];
         final textAnnotations = annotateImageResponse.textAnnotations;
+        final labelAnnotations = annotateImageResponse.labelAnnotations;
 
+        // Process text annotations
         if (textAnnotations != null && textAnnotations.isNotEmpty) {
           final extractedText = textAnnotations[0].description;
           if (extractedText != null && extractedText.isNotEmpty) {
@@ -105,19 +111,30 @@ class _VisionApiExampleState extends State<VisionApiExample> {
               _extractedText = extractedText;
             });
             print('Extracted Text: $extractedText');
-            return;
           }
         }
-      }
 
-      setState(() {
-        _extractedText = 'No text found';
-      });
+        // Process label annotations
+        if (labelAnnotations != null && labelAnnotations.isNotEmpty) {
+          final labels = labelAnnotations.map((label) => label.description)
+              .toList();
+          setState(() {
+            _labels = labels;
+          });
+          print('Labels: $labels');
+        }
+      } else {
+        setState(() {
+          _extractedText = 'No text found';
+          _labels = [];
+        });
+      }
     } catch (e, stackTrace) {
       print('Error during Vision API request: $e');
       print('Stack Trace: $stackTrace');
     }
   }
+
 
 
 
@@ -149,8 +166,19 @@ class _VisionApiExampleState extends State<VisionApiExample> {
             child: Text('Select Image'),
           ),
           SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _extractTextAndLabelsFromImage,
+            child: Text('Extract Text and Labels'),
+          ),
+          SizedBox(height: 20),
           Text(
             'Extracted Text: $_extractedText',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Labels: ${_labels.join(', ')}', // 변경
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18),
           ),
