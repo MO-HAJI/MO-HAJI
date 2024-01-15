914eb1d9a0ca7a572cf07e25382e594eb547a235
Jiyeon Na <skwldus@hanyang.ac.kr>
2024-01-15 16:10:32 +0900

    add: bottom navigation bar

---
+++ madcamp3/lib/screens/mypage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class mypage extends StatefulWidget {
+  const mypage({super.key});
+
+  @override
+  State<mypage> createState() => _mypageState();
+}
+
+class _mypageState extends State<mypage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
